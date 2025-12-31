a49318916efbcb109d54bb56e0f024eeab21f03c
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2025-12-31 10:51:48 +0530

    Finalizing the App

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    cacheComponents: true,
     images: {
         remotePatterns: [
             {
@@ -10,6 +14,20 @@ const nextConfig: NextConfig = {
             }
         ]
     },
+    async rewrites() {
+        return [
+            {
+                source: "/ingest/static/:path*",
+                destination: "https://us-assets.i.posthog.com/static/:path*",
+            },
+            {
+                source: "/ingest/:path*",
+                destination: "https://us.i.posthog.com/:path*",
+            },
+        ];
+    },
+    // This is required to support PostHog trailing slash API requests
+    skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
